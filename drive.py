b0f21af04675443ebf17b54050b449f10bcabc8d
Param Aggarwal <paramaggarwal@gmail.com>
2017-03-05 16:26:40 +0530

    modify drive code to emit outputs of the convolutional layers as base64 encoded images

--- drive.py
+++ drive.py
@@ -3,6 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
+import h5py
 
 import numpy as np
 import socketio
@@ -12,8 +13,8 @@ from PIL import Image
 from flask import Flask
 from io import BytesIO
 
-from keras.models import load_model
-import h5py
+from keras.models import Model, load_model
+from keras.layers import Merge
 from keras import __version__ as keras_version
 
 sio = socketio.Server()
@@ -21,9 +22,36 @@ app = Flask(__name__)
 model = None
 prev_image_array = None
 
+def image3dto2d(image_array):
+    images = []
+    for index in range(image_array.shape[2]):
+        layer_image = image_array[:, :, index].reshape(image_array.shape[0], image_array.shape[1])
+        images.append(layer_image)
+
+    return images;
+
+def imageArrayToBase64(image_array):
+    # print(image_array.shape, np.min(image_array), np.max(image_array), np.mean(image_array))
+
+    # normalize image values to be 0-255 and uint8
+    image_array = image_array - np.min(image_array)
+    image_array = image_array * (255.0 / np.max(image_array))
+    image_array = image_array.astype(np.uint8)
+    image = Image.fromarray(image_array , mode='L')
+
+    # print(image_array.shape, np.min(image_array), np.max(image_array), np.mean(image_array))
+    # image.save('test.png')
+
+    image_bytes = BytesIO()
+    image.save(image_bytes, format='JPEG')
+    image_base64 = base64.b64encode(image_bytes.getvalue())
+    image_string = image_base64.decode('utf-8')
+    return image_string
 
 @sio.on('telemetry')
 def telemetry(sid, data):
+    sio.emit('telemetry', data)
+
     if data:
         # The current steering angle of the car
         steering_angle = data["steering_angle"]
@@ -35,9 +63,11 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
-        min_speed = 10
-        max_speed = 20
+        model_prediction = model.predict(image_array[None, :, :, :], batch_size=1)
+        prediction = model_prediction[0]
+        steering_angle = float(prediction)
+        min_speed = 20
+        max_speed = 30
         if float(speed) < min_speed:
             throttle = 1.0
         elif float(speed) > max_speed:
@@ -47,6 +77,20 @@ def telemetry(sid, data):
 
         print(steering_angle, throttle)
         send_control(steering_angle, throttle)
+
+        layers_data = []
+        for layer in model_prediction[1:]:
+            layer_b64 = []
+            layer_images = image3dto2d(layer[0])
+            for layer_image in layer_images:
+                layer_image_b64 = imageArrayToBase64(layer_image)
+                layer_b64.append(layer_image_b64)
+            layers_data.append(layer_b64)
+        # print(layers_data)
+
+        sio.emit('prediction', {
+            "layerData": layers_data
+        })
 
         # save frame
         if args.image_folder != '':
@@ -100,6 +144,10 @@ if __name__ == '__main__':
             ', but the model was built using ', model_version)
 
     model = load_model(args.model)
+
+    # model which exposes internal layers
+    model = Model(input=model.input, output=[model.output, model.layers[0].output, model.layers[1].output, model.layers[2].output, model.layers[3].output, model.layers[4].output, model.layers[5].output]);
+    model.summary()
 
     if args.image_folder != '':
         print("Creating image folder at {}".format(args.image_folder))
